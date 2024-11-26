69baa399ac217e7d04f711e491fb3162824745fa
wanghao <qwe875707281@163.com>
2024-11-26 14:55:52 +0800

    feat: google登录方法名修改

--- sections/auth/google-auth-button.tsx
+++ sections/auth/google-auth-button.tsx
@@ -5,7 +5,7 @@ import { signIn } from 'next-auth/react';
 import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/icons';
 
-export default function GithubSignInButton() {
+export default function GoogleSignInButton() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl');
 
